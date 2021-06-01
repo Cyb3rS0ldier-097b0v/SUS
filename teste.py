556952da3e360dbf237bf36ee8e2983fddf740ab
Cyb3rS0ldier_097b0v <69645194+Cyb3rS0ldier-097b0v@users.noreply.github.com>
2021-06-01 17:10:39 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,33 +1,79 @@
 #!/bin/python3
 
-import os,sys,webbrowser,platform,time
+import os,sys,time
 
-def comeco():
-   os.system("clear")
-   print("mostrar todas as senhas? [s/n]")
-   pergunta = input(">>> ")
 
-def verificacao():
-     if platform.system() == "Windows":
-      webbrowser.open_new_tab("https://youtu.be/dQw4w9WgXcQ")
-     if platform.system() == "Linux":
-      os.system("termux-open-url https://youtu.be/dQw4w9WgXcQ")
+def SUS():
+      os.system("termux-open-url https://youtu.be/YFGZURDRsuE")
+
+
+
+def susNormal():
+      os.system("termux-open-url https://youtu.be/0bZ0hkiIKt0")
+
+
+
+def instalacao():
+     print("voce ja tem você já tem instalado o termux-api no seu termux?\nse não, deseja instalar? [s/jt (ja tenho)/n]")                       >
+     pergunta1 = input(">>> ")
+
+
 
 
 os.system("clear")
-print("mostrar todas as senhas? [s/n]")
+print("voce ja tem você já tem instalado o termux-api no seu termux?\nse não, deseja instalar? [s/jt (ja tenho)/n]")
+pergunta1 = input(">>> ")
+
+if pergunta1 == 's':
+        resposta = os.system("apt install termux-api -y")
+
+if pergunta1 == 's':
+        resposta = os.system("apt install figlet -y")
+
+if pergunta1 == 'n':
+        resposta = print("ok")
+
+if pergunta1 == 'n':
+        resposta = sys.exit()
+
+if pergunta1 == 'jt':
+        resposta = print("blz")
+
+
+
+else:
+        print("sua resposta não foi entendida\nresponda apenas com S para sim, N para nao e ja tenho para se voce ja                        >
+        time.sleep(9)
+        os.system("clear")
+        instalacao()
+
+
+def sus():
+        os.system("clear")
+        print("vc ta muito sus[s(sus)/n(não)]")
+
+time.sleep(2)
+os.system("clear")
+print("vc ta muito sus[s(sus)/n(não)]")
 pergunta = input(">>> ")
 
 if pergunta == 'n':
         resposta = print("ok, então thau")
 
+time.sleep(1)
 if pergunta == 'n':
-        resposta = sys.exit()
+        resposta = SUS()
 
 if pergunta == 's':
-        resposta = verificacao()
+        resposta = susNormal()
+
+if pergunta == 'sus':
+        resposta = susNormal()
 
 else:
         print("sua resposta não foi entendida\nresponda apenas com s (sim) ou n (não)")
         time.sleep(6)
-        comeco()
+        sus()
+
+os.system("clear")
+os.system("figlet SUS")
