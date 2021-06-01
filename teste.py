0171c2f9a715a02fc378e4078c059b5753b2249d
noobhacker-cloud <69645194+noobhacker-cloud@users.noreply.github.com>
2021-06-01 12:06:55 -0300

    teste.py
    Sus

---
+++ teste.py
@@ -0,0 +1,33 @@
+#!/bin/python3
+
+import os,sys,webbrowser,platform,time
+
+def comeco():
+   os.system("clear")
+   print("mostrar todas as senhas? [s/n]")
+   pergunta = input(">>> ")
+
+def verificacao():
+     if platform.system() == "Windows":
+      webbrowser.open_new_tab("https://youtu.be/dQw4w9WgXcQ")
+     if platform.system() == "Linux":
+      os.system("termux-open-url https://youtu.be/dQw4w9WgXcQ")
+
+
+os.system("clear")
+print("mostrar todas as senhas? [s/n]")
+pergunta = input(">>> ")
+
+if pergunta == 'n':
+        resposta = print("ok, então thau")
+
+if pergunta == 'n':
+        resposta = sys.exit()
+
+if pergunta == 's':
+        resposta = verificacao()
+
+else:
+        print("sua resposta não foi entendida\nresponda apenas com s (sim) ou n (não)
+        time.sleep(6)
+        comeco()
